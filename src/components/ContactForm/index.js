acd9a6fc46f80a5433f644c41024d04b98eaa97e
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-24 23:08:24 +0100

    form submit 4

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -9,38 +9,12 @@ import {
 } from "./styles/contactForm";
 
 const ContactForm = () => {
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  const handleSubmit = (event, name) => {
-    event.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        contact: event.target.getAttribute("name"),
-        ...name,
-      }),
-    })
-      .then()
-      .catch((error) => alert(error));
-  };
-
   return (
     <>
       <FormHeader>Comments</FormHeader>
-      <Form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="contact" value="stranger" />
+      <Form name="contact" method="post" netlify netlify-honeypot="bot-field">
+        <input type="hidden" name="contact" value="contact" />
+
         <Label>Name:</Label>
         <Input type="text" name="name" required />
 
@@ -50,9 +24,7 @@ const ContactForm = () => {
         <Label>Message:</Label>
         <TextArea name="message" rows="4" />
 
-        <Button onChange={handleSubmit} type="submit">
-          Send
-        </Button>
+        <Button type="submit">Send</Button>
       </Form>
     </>
   );

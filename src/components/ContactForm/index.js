33c0870b16f936acfd7459c1095ae36e65128936
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-24 22:38:59 +0100

    form submit debug

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -9,18 +9,46 @@ import {
 } from "./styles/contactForm";
 
 const ContactForm = () => {
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        contact: event.target.getAttribute("name"),
+        // ...name,
+      }),
+    })
+      .then()
+      .catch((error) => alert(error));
+  };
+
   return (
     <>
       <FormHeader>Comments</FormHeader>
-      <Form name="contact" method="POST" data-netlify="true">
+      <Form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="contact" value="stranger" />
         <Label>Name:</Label>
-        <Input type="text" name="name" required />
+        <Input type="text" name="name" onChange={handleSubmit} required />
 
         <Label>Email:</Label>
-        <Input type="email" name="email" />
+        <Input type="email" name="email" onChange={handleSubmit} />
 
         <Label>Message:</Label>
-        <TextArea name="message" rows="4"></TextArea>
+        <TextArea name="message" rows="4" onChange={handleSubmit} />
 
         <Button type="submit">Send</Button>
       </Form>

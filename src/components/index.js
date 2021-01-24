efcfb4e7fe921845c22c3ec321d58f15001ab0c7
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-24 22:24:55 +0100

    filename debug part 3

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Projects } from "./Projects";
 export { default as Navbar } from "./Navbar";
 export { default as HeroSection } from "./HeroSection";
-export { default as InfoSection } from "./InfoSection";
+export { default as InfoSection } from "./infoSection";
 export { default as Footer } from "./Footer";

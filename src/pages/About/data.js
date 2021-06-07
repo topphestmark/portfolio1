63f2f5c84e016af05f4e2d3d869c3a2d49993fcc
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-06-07 22:03:12 +0200

    About me bio update

--- src/pages/About/data.js
+++ src/pages/About/data.js
@@ -5,6 +5,6 @@ export const aboutObj = {
   paragraph1:
     "My name is Top, or Suphachai Hestmark as my real name. I have a background from the oil industry and have a strong desire to transform my career into the tech industry.",
   paragraph2:
-    "I started learning programming in 2019 and decided to make the path to become a full time programmer. I currently enrolling Noroff Vocational School with in the Frontend Development and expecting to be gratuated late 2021. A part from study I love to spending my spare-time exploring other technologies like back-end and blockchain.",
+    "I started learning programming in 2019 and decided to make the path to become a full time programmer. I currently enrolling Noroff Vocational School with in the Frontend Development and expecting to gratuate late 2021. A part from study I love to spending my spare-time exploring other technologies of the back-end and blockchain to see what problem those technologies solve.",
   image: profileImage,
 };

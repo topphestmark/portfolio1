283e688ca8e8eccdedb34452a1d676b89b39cc7a
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-06-07 18:58:01 +0200

    Updated icons and add 1 new project-card

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -14,6 +14,7 @@ export const Nav = styled.nav`
 
   background: ${({ scrollNav }) =>
     scrollNav ? theme.colors.light : "transparent"};
+
   ${({ scrollNav }) => (scrollNav ? theme.navShadow : "")}
 
   @media screen and (max-width: 960px) {
--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,11 +1,11 @@
 import arkanoid from "../../images/icons/arkanoid.svg";
-import candlestick from "../../images/icons/candlestick.svg";
 import vaccine from "../../images/icons/vaccine.svg";
 import shopping from "../../images/icons/shopping.svg";
 import social_media from "../../images/icons/social-media.svg";
 import startup from "../../images/icons/startup.svg";
 import stock from "../../images/icons/stock.svg";
 import streaming from "../../images/icons/streaming.svg";
+import online_shopping from "../../images/icons/online-shopping.svg";
 
 export const projects = [
   {
@@ -18,7 +18,7 @@ export const projects = [
   },
   {
     id: "2",
-    icon: candlestick,
+    icon: stock,
     title: "Crypto Chart",
     description: "Top 100 cryptocurrency chart per current market.",
     stack: ["NextJS", "CSS", "Coingecko API"],
@@ -45,7 +45,7 @@ export const projects = [
     icon: social_media,
     title: "Dev Connector",
     description: "Full-stack social media application for developers.",
-    stack: ["ReactJS", "NodeJS", "Express", "MongoDB", "Redux"],
+    stack: ["ReactJS", "NodeJS", "Express", "MongoDB", "Redux", "Bootstrap"],
     link: "https://dev-connector-vs-555.herokuapp.com/",
   },
   {
@@ -60,18 +60,16 @@ export const projects = [
     id: "7",
     icon: streaming,
     title: "Netflix TMDB",
-    description:
-      "All the others do and nobody ever gets punished for doing so.",
+    description: "Netflix movie banner UI from 2020.",
     stack: ["ReactJS", "Styled-components", "Firebase", "Axios", "TMDb"],
     link: "https://netflix-tmdb.firebaseapp.com",
   },
   {
     id: "8",
-    icon: stock,
-    title: "TBA",
-    description:
-      "All the others do and nobody ever gets punished for doing so.",
-    stack: ["HTML", "CSS", "Javascript"],
-    link: "/",
+    icon: online_shopping,
+    title: "Nordic Swag",
+    description: "Vanilla javascript e-commerce project with CRUD operation.",
+    stack: ["HTML", "Sass", "Javascript", "Strapi"],
+    link: "https://nordic-swag.netlify.app/index.html",
   },
 ];

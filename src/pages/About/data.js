2aa8825a8794a4bbbe37c795a4c9e21e97afff7a
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-08-11 15:20:24 +0200

    Correcting settings

--- src/pages/About/data.js
+++ src/pages/About/data.js
@@ -3,7 +3,7 @@ import profileImage from "../../images/profile_image.jpg";
 export const aboutObj = {
   header: "ABOUT ME",
   paragraph1:
-    "My name is Top, or Suphachai Hestmark as my real name. I have a background from the oil industry and have a strong desire to transform my career into the tech industry. To be able to combine my creativity with technical problem solving has been my biggest drive to do programming.",
+    "My name is Top, or Suphachai Hestmark as my real name. I have a background from the oil industry and have a strong desire to transform my career into the tech industry. Being able to combine my creativity with technical problem solving has been my biggest drive to continue programming.",
   paragraph2:
     "I started learning programming in 2019 and decided to make the path to become a full time programmer. I currently enrolling Noroff Vocational School with in the Frontend Development and expecting to gratuate late 2021. A part from study I love to spending my spare-time exploring other technologies as well and curious about what problem they solve.",
   image: profileImage,
